7695b15adcfce9954e2ef115b3bbf5fcc25115bb
LuongNM <luongnm.23ba14184@usth.edu.vn>
2025-12-19 21:55:27 +0700

    P01_03

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,31 +1,62 @@
-# Input a number
+import json
+import os
+
+
+def save_data(students, courses):
+    with open("students.json", "w") as f:
+        json.dump(students, f, indent=4)
+
+    with open("courses.json", "w") as f:
+        json.dump(courses, f, indent=4)
+
+
+def load_data():
+    if os.path.exists("students.json"):
+        with open("students.json", "r") as f:
+            students = json.load(f)
+    else:
+        students = []
+
+    if os.path.exists("courses.json"):
+        with open("courses.json", "r") as f:
+            courses = json.load(f)
+    else:
+        courses = []
+
+    return students, courses
+
+
+# ============================
+# ORIGINAL FUNCTIONS
+# ============================
+
 def input_number(unit):
     return int(input(f"Enter the number of {unit} in this class: "))
 
-# Ask the user to enter info for an item
+
 def input_infos(item_type, infos):
     item = {}
     for info in infos:
         item[info] = input(f"\tEnter the {item_type}'s {info}: ")
     return item
 
-# Display list of items
+
 def display(lst):
     for i, item in enumerate(lst):
         print(f"{i+1}. {item}")
 
-# Input the student mark in a course
+
 def input_mark(student, course_id):
     if "marks" not in student:
         student["marks"] = {}
     student["marks"][course_id] = input("Enter the student's mark for this course: ")
 
-# List students
+
 def list_students(students):
     if len(students) == 0:
         print("There aren't any students yet")
         return
-    
+
     print("==> Here is the student list:")
     for i, student in enumerate(students):
         print(f"{i+1}. {student['id']} {student['name']} {student['DoB']}", end="")
@@ -36,7 +67,7 @@ def list_students(students):
                 print(f"({course_id} {mark})", end="\t")
         print()
 
-# List courses
+
 def list_courses(courses):
     if len(courses) == 0:
         print("There aren't any courses yet")
@@ -46,7 +77,7 @@ def list_courses(courses):
     for i, course in enumerate(courses):
         print(f"{i+1}. {course['id']} {course['name']}")
 
-# Select an option safely
+
 def select(options, msg="Select: "):
     while True:
         try:
@@ -57,30 +88,40 @@ def select(options, msg="Select: "):
             pass
         print("Invalid input. Try again.")
 
-# Fake pause to match teacher style
+
 def pause():
     input("Press Enter to continue...")
 def main():
-    students = []
-    courses = []
-    num_students = 0
-    num_courses = 0
+    # Load saved data
+    students, courses = load_data()
+
+    num_students = len(students)
+    num_courses = len(courses)
 
     while True:
-        print("\n===== Student Mark Management =====")
-        print("1. Input number of students & student info")
-        print("2. Input number of courses & course info")
-        print("3. Select course & input marks")
-        print("4. List students")
-        print("5. List courses")
-        print("6. Show student marks for a course")
+        print("====================================")
+        print("Here are the functions of this application")
         print("0. Exit")
+        print("1. Input number of students")
+        print("2. Input students information (id, name, DoB)")
+        print("3. Input number of courses")
+        print("4. Input course information (id, name)")
+        print("5. Input marks for student in a course")
+        print("6. List courses")
+        print("7. List students")
+        print("====================================")
+
         selection = select(range(0, 8))
+
         if selection == 0:
+            print("Saving data...")
+            save_data(students, courses)
+            print("Data saved. Bye!")
             break
+
         elif selection == 1:
-          num_students = input_number("students")
-          print(f"==> There are {num_students} student(s) in this class")
+            num_students = input_number("students")
+            print(f"==> There are {num_students} student(s) in this class")
 
         elif selection == 2:
             if num_students == 0:
@@ -92,7 +133,7 @@ def main():
             for i in range(num_students):
                 print(f"Student No. {i+1}")
                 students.append(input_infos("student", ("id", "name", "DoB")))
-            
+
             list_students(students)
 
         elif selection == 3:
@@ -111,6 +152,7 @@ def main():
                 courses.append(input_infos("course", ("id", "name")))
 
             list_courses(courses)
+
         elif selection == 5:
             list_courses(courses)
             if len(courses) > 0:
@@ -127,10 +169,6 @@ def main():
         elif selection == 7:
             list_students(students)
 
-        else:
-            print("Invalid input. Please try again.")
         pause()
-
-
 if __name__ == "__main__":
     main()
